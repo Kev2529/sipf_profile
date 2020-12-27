8d2e99ad5403d4661e2335e7f804c964781611ee
Cyril VINH-TUNG <cyril@invitu.com>
2020-12-26 17:35:58 -1000

    [IMP] analytic account and tags can be defined at purchase_requisition and they get propaged to lines

--- models/purchase_requisition.py
+++ models/purchase_requisition.py
@@ -13,12 +13,46 @@ class PurchaseRequisition(models.Model):
         comodel_name='hr.department', string='Department',
         default=_get_my_department,
         help='Select the Department the purchase requisition is for')
+    account_analytic_id = fields.Many2one(
+        'account.analytic.account', string='Analytic Account',
+        compute='_compute_analytic_account',
+        inverse='_inverse_analytic_account',
+        help='This account will be propagated to all lines, if you need '
+        'to use different accounts, define the account at line level.',
+    )
+    analytic_tag_ids = fields.Many2many(
+        'account.analytic.tag', string='Analytic Tags',
+        inverse='_inverse_analytic_tags',
+        help='This account will be propagated to all lines, if you need '
+        'to use different accounts, define the account at line level.',
+    )
 
     @api.onchange('user_id')
     def onchange_user_id(self):
         employees = self.user_id.employee_ids
         self.department_id = (employees[0].department_id if employees
                               else self.env['hr.department'] or False)
+
+    @api.depends("line_ids.account_analytic_id")
+    def _compute_analytic_account(self):
+        for rec in self:
+            account = rec.mapped("line_ids.account_analytic_id")
+            if len(account) == 1:
+                rec.account_analytic_id = account.id
+            else:
+                rec.account_analytic_id = False
+
+    def _inverse_analytic_account(self):
+        for rec in self:
+            if rec.account_analytic_id:
+                for line in rec.line_ids:
+                    line.account_analytic_id = rec.account_analytic_id
+
+    def _inverse_analytic_tags(self):
+        for rec in self:
+            if rec.analytic_tag_ids:
+                for line in rec.line_ids:
+                    line.analytic_tag_ids = rec.analytic_tag_ids
 
 
 class PurchaseRequisitionLine(models.Model):
